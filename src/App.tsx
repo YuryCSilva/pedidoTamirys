efed7a4b4911753280049d4e291806178e93b189
Yury Silva <ysilva@marlin.com>
2023-02-08 22:48:18 -0300

    feat: add providers in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import { RouterProvider } from 'react-router-dom';
+import { createContext, useState } from 'react';
 import Routes from 'routes';
+import { ThemeProvider } from 'styled-components';
+import theme from 'ui-theme/theme';
+import GlobalStyle from 'utils/global';
+
+type ContextProps = {
+  isAuthenticated: boolean,
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const Context = createContext({} as ContextProps)
 
 const App = () => {
-  return <RouterProvider router={Routes} />;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes />
+      </ThemeProvider>
+    </Context.Provider>
+  );
 };
 
 export default App;
